cb62544143a96d95a43d358106774a05f477f85d
bnbump3131 <116858121+bnbump3131@users.noreply.github.com>
2023-12-05 04:12:29 -0500

    moved and added code to allow for fragment navigation

---
+++ app/src/main/java/com/zybooks/cardmatch/DifficultyFragment.java
@@ -0,0 +1,18 @@
+package com.zybooks.cardmatch;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class DifficultyFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_difficulties, container, false);
+    }
+}
