de1a59d57cf2ac99dc2d1bb5c10a24802109ce46
remullis789 <remullis789@gmail.com>
2023-12-05 21:32:08 -0500

    created fragments for medium and hard difficulties

--- app/src/main/java/com/zybooks/cardmatch/HardDifficulty.java
+++ app/src/main/java/com/zybooks/cardmatch/HardDifficulty.java
@@ -12,7 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-public class GameFragment extends Fragment {
+public class HardDifficulty extends Fragment {
     private CardGame mGame;
     private GridLayout mCardGrid;
     //private TextView mScore;
@@ -22,12 +22,12 @@ public class GameFragment extends Fragment {
     View[] selectedViewsPair = new View[2];
     int selectedViewsCounter;
 
-    public GameFragment() {
+    public HardDifficulty() {
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View parentView = inflater.inflate(R.layout.fragment_game, container, false);
+        View parentView = inflater.inflate(R.layout.fragment_hard_difficulty, container, false);
 
         mCardFaceColor[0] = ContextCompat.getColor(this.requireActivity(), R.color.red);
         mCardFaceColor[1] = ContextCompat.getColor(this.requireActivity(), R.color.light_orange);
@@ -53,7 +53,7 @@ public class GameFragment extends Fragment {
         Button newGameBtn = parentView.findViewById(R.id.new_game_button);
         newGameBtn.setOnClickListener(v -> onNewGameClick(parentView));
 
-        mGame = new CardGame(3, 4);
+        mGame = new CardGame(4, 5);
         startGame();
 
         return parentView;
@@ -68,7 +68,7 @@ public class GameFragment extends Fragment {
  */
 
     private void startGame() {
-        mGame.newGame(mGame.GRID_HEIGHT, mGame.GRID_WIDTH);
+        mGame.newGame(4, 5);
         setCardsFaceDown();
     }
 
