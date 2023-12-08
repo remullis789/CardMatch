05c417812f6ba641638e73ab69dd389d047c556d
mollyyreid <mrharr5384@ung.edu>
2023-12-08 18:39:54 -0500

    changed some button interfaces

--- app/src/main/java/com/zybooks/cardmatch/helpFragment.java
+++ app/src/main/java/com/zybooks/cardmatch/helpFragment.java
@@ -1,9 +1,11 @@
 package com.zybooks.cardmatch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
